1c49304d02ca6bff4e50ea3de71494a4039c4a62
liugm <824614513@qq.com>
2017-02-16 14:46:49 +0800

    加入数据库和表的各项设置

---
+++ app/src/main/java/com/myfounder/coolweather/MyApplication.java
@@ -0,0 +1,13 @@
+package com.myfounder.coolweather;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by liu_guomeng on 2017/2/16.
+ */
+public class MyApplication extends LitePalApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
