f4d8a2ddbffc9be7d2d39f29d8dcbc7a0c71b560
liugm <824614513@qq.com>
2017-02-17 17:09:21 +0800

    天气界面布局

--- app/src/main/java/com/myfounder/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/myfounder/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.myfounder.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -109,7 +110,7 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    /*if (getActivity() instanceof MainActivity) {
+                    if (getActivity() instanceof MainActivity) {
                         Intent intent = new Intent(getActivity(), WeatherActivity.class);
                         intent.putExtra("weather_id", weatherId);
                         startActivity(intent);
@@ -118,9 +119,10 @@ public class ChooseAreaFragment extends Fragment {
                         WeatherActivity activity = (WeatherActivity) getActivity();
                         activity.drawerLayout.closeDrawers();
                         activity.swipeRefresh.setRefreshing(true);
+                        activity.setWeatherId(weatherId);
                         activity.requestWeather(weatherId);
-                    }*/
-                    queryCounties();
+                    }
+                    //queryCounties();
                 }
             }
         });
